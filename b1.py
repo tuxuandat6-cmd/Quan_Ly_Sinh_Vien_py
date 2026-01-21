4ddd85c528d44eb599ab5e96a41cec0bde53b6c8
tuxuandat6-cmd <tuxuandat6@gmail.com>
2026-01-21 09:03:48 +0700

    Delete b1.py

--- b1.py
+++
@@ -1,8 +0,0 @@
-# import thư viện tkinter
-import tkinter as tk
-root = tk.Tk()   # tạo cửa sở chính, Tk() tạo cửa sổ gốc, root bién đại diện cho toàn cửa sổ
- # Creating a label widget
-myLabel = tk.Label(root, text='Hello World')   #hiện thị chữm root là có label nằm trong
- # shovingit onto the screen
-myLabel.pack(expand = True)  #pack() quanr lý bố cục, nếu kh có thif sex kh hiển thị, expand = True cho phép widget mở rộng
-root.mainloop()  #chạy vòng lặp giao diện, nếu kh có thì chương trình chạy xong sẽ tắt luôn
